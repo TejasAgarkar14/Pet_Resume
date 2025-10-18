0e5debdd4a90a4d121359739576ec2081218043d
TejasAgarkar14 <tejasstudy14@gmail.com>
2025-10-18 14:42:03 +0530

    Deployement issue fixed_try_1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Tell Vite not to try to bundle these at build time
+      external: ['jspdf', 'html2canvas']
+    }
+  },
+  optimizeDeps: {
+    // Ensure they are pre-bundled properly in dev & build
+    include: ['jspdf', 'html2canvas']
+  }
 })
